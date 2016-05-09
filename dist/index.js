b19d10e3880d0f68b62764bcaf0779272d784120
Tseng, Taiwei | Davis | ASRHQ <taiwei.tseng@rakuten.com>
2016-05-09 19:14:19 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+exports.default = function () {
+    this.init = function () {
+        var jsdom = require("jsdom-no-contextify").jsdom;
+        global.document = jsdom("virtual dom");
+        global.window = document.parentWindow;
+        global.navigator = {};
+        global.navigator.userAgent = {
+            indexOf: function indexOf() {},
+            toLowerCase: function toLowerCase() {
+                return "mozilla/5.0 (macintosh; intel mac os x 10_10_2) applewebkit/537.36 (khtml, like gecko) chrome/43.0.2357.65 safari/537.36";
+            },
+            match: function match() {
+                return "";
+            }
+        };
+        //set lodash
+        global._ = _;
+        //set jquery
+        require("jquery1-7-1");
+    };
+
+    this.render = function (config) {
+        var Component = _react2.default.createFactory(require(config.componentPath));
+        var option = config.beforeRender();
+        var ComponentMarkup = _react2.default.renderToStaticMarkup(Component(config.props));
+        return config.afterRender(ComponentMarkup, option);
+    };
+};
+
+require("babel-polyfill");
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+require('node-jsx').install({
+    extension: '.jsx'
+});
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import "babel-polyfill";
+import React from "react";
+require('node-jsx').install({
+    extension: '.jsx'
+});
+
+
+export default function(){
+	this.init = function(){
+		var jsdom = require("jsdom-no-contextify").jsdom;
+        	global.document = jsdom("virtual dom");
+        	global.window = document.parentWindow;
+        	global.navigator = {};
+        	global.navigator.userAgent = {
+            		indexOf: function () {},
+            		toLowerCase: function () {
+                		return "mozilla/5.0 (macintosh; intel mac os x 10_10_2) applewebkit/537.36 (khtml, like gecko) chrome/43.0.2357.65 safari/537.36";
+            		},
+            		match: function () {
+                		return "";
+            		}
+        	};
+        	//set lodash
+        	global._ = _;
+        	//set jquery
+        	require("jquery1-7-1");
+	}
+
+	this.render = function(config){
+		var Component = React.createFactory(require(config.componentPath));
+		var option = config.beforeRender();
+		var ComponentMarkup = React.renderToStaticMarkup(Component(config.props));
+		return config.afterRender(ComponentMarkup, option);
+	}	
+
+}
+
+
+
